7b909d93a9200615e43f182cab839c7dc3792d7a
Brian Carey <biscotty666@gmail.com>
2022-06-26 16:04:02 -0600

    Corrected output of txt and moved the name modification function to the top

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ pdfmetrics.registerFont(TTFont('Vampire', 'Vampire Wars.ttf'))
 #Get current working directory
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
-print(f"{cwd}\n-----")
+# print(f"{cwd}\n-----")
 
 # Fetch configuration variables
 with open('bot_config//config.yml') as file :
@@ -45,18 +45,32 @@ with open('bot_config//config.yml') as file :
 creds = settings(config['CredName'], config['CredPass'], config['CredNum'], config['CredLet'])
 client = SWGOHhelp(creds)
 
+# This is to change the returned name into the name as printed
+# on the poster
+def GNameModify(name):
+    # Remove the word Phantom and any of it's permutations
+    name = regex.sub(r'Phant[oø?]+m\s*?', '', name, 1)
+    # Remove leading space
+    name = regex.sub(r'^ ', '', name, 1)
+    # Special bonus for Limb
+    name = regex.sub(r'\?\?', 'i', name, 1)
+    return name
+
+
 #Fetch the data
 def GetGuildData():
     # Initialize some variables
     counter = 0
     guilds = []
     now = datetime.now()
-
-    # Create txt file
+ 
+    # Initialize txt file with some header info
     f = open('OpenSlots.txt', 'w')
-    f.write("Updated: " + str(now) + '\n')
+    f.write("Updated: " + str(now) + '\n\n')
     f.write('Name'.ljust(20) + 'GM'.rjust(7) + 'GP'.rjust(8) + '\n')
-    f.close()
+    f.close
+
+    # rt pdf to png
 
     # Make the calls and populate guilds
     for allycode in config['allycodes'] :
@@ -79,13 +93,15 @@ def GetGuildData():
         # Add the rest of the info
         guilds.append({
             'GGp' : guildinfo['gp'],
-            'GName' : guildinfo['name'],
+            'GName' : GNameModify(guildinfo['name']),
             'GMembers' : guildinfo['members'],
         })
 
         # Add infor to the OpenSlots.txt
         f = open('OpenSlots.txt', "a")
-        if guildinfo['members'] < 50 : f.write(str(guildinfo['name']).ljust(20) + str(guildinfo['members']).rjust(7) + str(round(guildinfo['gp']/1000000)).rjust(8)+'\n')
+        if guildinfo['members'] < 50:
+            # f.write(str(guilds['GName']).ljust(20) + str(guilds['GMembers']).rjust(7) + str(round(guilds['GGp']/1000000)).rjust(8) + '/n')
+            f.write(str(GNameModify(guildinfo['name'])).ljust(20) + str(guildinfo['members']).rjust(7) + str(round(guildinfo['gp']/1000000)).rjust(8)+'\n')
         f.close()
 
     return guilds
@@ -102,16 +118,23 @@ def GenerateSlide():
     height = 360
     c.setPageSize((width, height))
     c.setTitle(config['RSTitle'])
+    im = Image.open('RPBackground.jpg')
+    im_rgba = im.copy()
+    im_rgba.putalpha(200)
+    im_rgba.save('RPBackground.png')
 
     #Draw statically positioned items on canvas
-    c.drawImage('RPBackground.jpg', 0, 0, width=width, height=height)
+
+    c.drawImage('RPBackground.png', 0, 0, width=width, height=height)
+    # c.drawImage('image.jpg', width=width, height=height)
+
     c.setFont(config['Font'], config['FontSize'])
-    c.setFillColor(colors.beige)
+    c.setFillColor(colors.orangered)
     c.drawCentredString(width/2, 330, config['RSTitle'])
     c.setFont(config['Font'], config['FontSize']-10)
     c.drawCentredString(width/2, 10, 'hello there')
-    c.drawImage('Shard-Character-Ki-Adi-Mundi.png', 437, 300,width=15, height=15)
-    c.drawImage('Shard-Character-Wat_Tambor.png', 512, 300,width=15, height=15)
+    c.drawImage('Shard-Character-Ki-Adi-Mundi.png', 440, 300,width=15, height=15)
+    c.drawImage('Shard-Character-Wat_Tambor.png', 515, 300,width=15 , height=15)
 
     #populate data for the table
     #create first row of table
@@ -122,18 +145,11 @@ def GenerateSlide():
         #modify appearance and content of items in guilds
         GPRounded = str(round(guild['GGp']/1000000))
 
-        # This is to change the returned name into the name as printed
-        # on the poster
-        def GNameModify(name):
-        # Remove the word Phantom and any of it's permutations
-            name = regex.sub(r'Phant[oø?]+m\s*?', '', str(guild['GName']), 1)
-        # Remove leading space
-            name = regex.sub(r'^ ', '', name, 1)
-        # Special bonus for Limb
-            name = regex.sub(r'\?\?', 'i', name, 1)
-            return name
+ 
+        GNameModified = GNameModify(guild['GName'])
+        
+        # GName = GNameModify('GName')
 
-        GNameModified = GNameModify('GName')
         DSTB = config['DSTB'][GNameModified]
         LSTB = config['LSTB'][GNameModified]
         CPIT = config['CPIT'][GNameModified]
@@ -146,6 +162,7 @@ def GenerateSlide():
     #create Table object based on data and add to image
 
     bcolor = Color(10,10,10,alpha=.5)
+    # rgbYellow = Color()
 
     table = Table(data, colWidths=[100,75,75,75,75,75,75], rowHeights=20)
     table.setStyle(TableStyle([
@@ -157,25 +174,27 @@ def GenerateSlide():
         ('TEXTCOLOR',(0,0),(-1,-1),colors.white),
         ('BOTTOMPADDING',(0,0),(-1,0),5),
         # set font and color for top row
-        ('FONTSIZE',(0,0),(-1,0),16),
-        ('TEXTCOLOR',(0,0),(-1,0),colors.beige),
+        ('FONTSIZE',(0,0),(-1,0),14),
+        ('TEXTCOLOR',(0,0),(-1,0),colors.blue),
         # set font size and color for first column
         ('FONTSIZE',(0,1),(0,-1),14),
-        ('TEXTCOLOR',(0,1),(0,-1),colors.greenyellow),
+        ('TEXTCOLOR',(0,1),(0,-1),colors.blue),
         # set font size and color for second column
         ('FONTSIZE',(1,1),(1,-1),12),
-        ('TEXTCOLOR',(1,1),(1,-1),colors.red),
-
+        ('TEXTCOLOR',(1,1),(1,-1),colors.yellow),
+        # set font size and color for third and fourth column
+        ('FONTSIZE',(2,1),(3,-1),12),
+        ('TEXTCOLOR',(2,1),(3,-1),colors.yellow),
         # set font size and color for fifth column
         ('FONTSIZE',(4,1),(4,-1),12),
-        ('TEXTCOLOR',(4,1),(4,-1),colors.pink),
-        ('BACKGROUND',(4,1),(4,-1),bcolor),
+        ('TEXTCOLOR',(4,1),(4,-1),colors.yellow),
+        # ('BACKGROUND',(4,1),(4,-1),bcolor),
         # set font size and color for sixth and seventh columns
         ('FONTSIZE',(5,1),(6,-1),12),
-        ('TEXTCOLOR',(5,1),(6,-1),colors.aqua),
+        ('TEXTCOLOR',(5,1),(6,-1),colors.yellow),
         # set font size and color for eighth column
         ('FONTSIZE',(7,1),(7,-1),12),
-        ('TEXTCOLOR',(7,1),(7,-1),colors.red),
+        ('TEXTCOLOR',(7,1),(7,-1),colors.yellow),
         # ('ALIGN',(1,1), (1,-1),'RIGHT'),
         ('ALIGN',(1,0), (-1,-1),'CENTER'),
         ]))
@@ -197,15 +216,15 @@ def Makepng():
 
 #Defining a few things
 secret_file = json.load(open(cwd+'/bot_config/secrets.json'))
-RecBot = commands.Bot(command_prefix='/', case_insensitive=True)#, owner_id=271612318947868673)
+RecBot = commands.Bot(command_prefix='!', case_insensitive=True)#, owner_id=271612318947868673)
 RecBot.config_token = secret_file['token']
 logging.basicConfig(level=logging.INFO)
 RecBot.version = "0.1.0"
 
 @RecBot.event
 async def on_ready():
-    print(f"-----\nLogged in as: {RecBot.user.name} : {RecBot.user.id}\n-----\nMy current prefix is: /\n-----")
-    await RecBot.change_presence(activity=discord.Game(name=f"Hi, my names {RecBot.user.name}.\nUse / to interact with me!")) # This changes the bots 'activity'
+    print(f"-----\nLogged in as: {RecBot.user.name} : {RecBot.user.id}\n-----\nMy current prefix is: !\n-----")
+    await RecBot.change_presence(activity=discord.Game(name=f"Hi, my names {RecBot.user.name}.\nUse ! to interact with me!")) # This changes the bots 'activity'
 
 @RecBot.command()
 async def echo(ctx, *, message=None):
